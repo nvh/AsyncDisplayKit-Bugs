b78cb206a7382f848bcb2a2204661492e7f697f8
Niels van Hoorn <nvh@nvh.io>
2016-01-26 08:34:38 +0100

    Added subclass of multiplex image node
    Showing error at compile time

---
+++ AsyncDisplayKit-Bugs/ExtendedMultiplexImageNode.swift
@@ -0,0 +1,13 @@
+//
+//  ExtendedMultiplexImageNode.swift
+//  AsyncDisplayKit-Bugs
+//
+//  Created by Niels van Hoorn on 26/01/16.
+//  Copyright © 2016 Zeker Waar. All rights reserved.
+//
+
+import AsyncDisplayKit
+
+class ExtendedMultiplexImageNode: ASMultiplexImageNode {
+    
+}
